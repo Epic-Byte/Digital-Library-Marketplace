18f825752d370eb44de1826b4851be7ce10937f3
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2022-05-13 17:01:40 +0100

    First commit

--- packages/hardhat/contracts/Library.sol
+++ packages/hardhat/contracts/Library.sol
@@ -1,9 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.12;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Library
 {
+
+    AggregatorV3Interface internal priceFeed;
     /*
     *@notice Instance of Library Item
     */
@@ -13,6 +16,7 @@ contract Library
         string name;
         string Link;
         string description;
+        string category;
         address seller;
         uint256 price;
 
@@ -21,28 +25,31 @@ contract Library
     /*
     *@notice Id counter 
     */
-    uint256 public count = 0;
-    uint256 public Pcount = 0;
+    uint256 public privateCount = 0;
+    uint256 public publicCount = 0;
 
 
-    address private Owner;
+    address private owner;
 
     /*
     *@notice maps user to their library
     */
-    mapping (address=>mapping(uint256=>content)) userLib;
-    mapping (address=>content[])privlib;
+    mapping(address=>mapping(uint256=>content)) userLib;
+    mapping(address=>content[]) privlib;
+    mapping(uint256 => content) public getContentById;
 
     /**
     @notice Events to log public library
     */
-    event PublicUpload(string _name, string _Link, string _description, uint256 price);
-    event Share(address sharer, string _filename, address _to);
-    event bought(address seller, address buyer, string name, uint256 price);
+    event PublicUpload(string _name, string _Link, string _description, string _category, uint256 price);
+    event Share(address _sharer, string _filename, address _to);
+    event bought(address _seller, address _buyer, string _name, uint256 _price);
 
 
-    error incorrect_ether();
-    error not_owner();
+    error incorrectPrice(uint256 price, uint256 paid);
+    error not_owner(address rightAddress, address yourAddress);
+    error incorrectId();
+
     
 
     /*
@@ -50,23 +57,30 @@ contract Library
     */
     content[] public publicLib;
 
+    /**
+     * Network: Rinkeby
+     * Aggregator: ETH/USD
+     * Address: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+     */
     constructor(address _owner)
     {
-        Owner= _owner;
+        owner= _owner;
+        priceFeed =  AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
     }
 
     /*
     *@notice uploads privately to users library
-     @param _name file name
-    @param _Link IPFS Link
-    @param _description file description
+    *@param _name file name
+    *@param _Link IPFS Link
+    *@param _description file description
+    *@param category file category
     */
-    function PrivateUpload(string calldata _name, string calldata _Link, string calldata _description) external
+    function PrivateUpload(string calldata _name, string calldata _link, string calldata _description, string calldata _category) external
     {
-        count++;
-        uint256 Count = count;
-        userLib[msg.sender][count]=content(Count,_name, _Link, _description, msg.sender,0);
-        privlib[msg.sender].push(content(Count,_name, _Link,_description,msg.sender,0));
+        privateCount++;
+        uint256 _privateCount = privateCount;
+        userLib[msg.sender][privateCount]=content(_privateCount, _name, _link, _description, _category, msg.sender, 0);
+        privlib[msg.sender].push(content(_privateCount, _name, _link, _description, _category, msg.sender, 0));
     }
 
     /**
@@ -74,14 +88,15 @@ contract Library
     @param _name file name
     @param _Link IPFS Link
     @param _description file description
+    @param _category file category
     */
-    function publicUpload(string calldata _name, string calldata _Link, string calldata _description) external returns(string memory)
+    function publicUpload(string calldata _name, string calldata _Link, string calldata _description, string calldata _category) external returns(string memory)
     {
-        Pcount++;
-        uint256 pcount = Pcount;
-        content memory Content = content(pcount,_name, _Link, _description,msg.sender,0);
+        publicCount++;
+        uint256 pubCount = publicCount;
+        content memory Content = content(pubCount,_name, _Link, _description, _category, msg.sender,0);
         publicLib.push(Content);
-        emit PublicUpload(_name, _Link, _description,0);
+        emit PublicUpload(_name, _Link, _description, _category, 0);
          return ("Added to Public Library");
     }
 
@@ -98,8 +113,8 @@ contract Library
         for(uint256 i=0; i< length; ) {
         require(_to[i] != address(0),"you cant share to zero address");
         
-        userLib[_to[i]][_ID] = content(c.ID, c.name, c.Link, c.description, c.seller,c.price);
-        privlib[_to[i]].push(content(c.ID,c.name, c.Link,c.description,c.seller,c.price));
+        userLib[_to[i]][_ID] = content(c.ID, c.name, c.Link, c.description, c.category, c.seller,c.price);
+        privlib[_to[i]].push(content(c.ID,c.name, c.Link,c.description, c.category, c.seller,c.price));
         emit Share(msg.sender, c.name, _to[i]);
         unchecked {i++; }
         }
@@ -118,47 +133,90 @@ contract Library
     /*
     *@notice make private item public for sale
     @param _ID id of item to make public
-    @param price price of item
+    @param _price price of item
     */
-    function publicSale(uint256 _ID, uint256 price)external  //note this was our make public funtion
+    function publicSale(uint256 _ID, uint256 _price)external  //note this was our make public funtion
     {
+     //   if(_ID != )
         content memory c = userLib[msg.sender][_ID];
-        publicLib.push(content(c.ID,c.name,c.Link,c.description,msg.sender,price));
-        emit PublicUpload(c.name, c.Link, c.description, price);
+        publicLib.push(content(c.ID, c.name, c.Link, c.description, c.category, msg.sender, _price));
+        emit PublicUpload(c.name, c.Link, c.description, c.category, _price);
     }
 
     /*
     *@notice buy an item from the public marketplace 
-    @param arrayID id of item in public library array
+    *@param arrayID the position of the item in the public items array
     */
-    function buyItem(uint256 arrayID)external payable 
-    // guys we have to test this to ensure enough gas is calculated by metamask to also execute he transfer
+    function buyItem(uint256 _arrayID) external payable
+    // guys we have to test this to ensure enough gas is calculated by metamask to also execute the transfer
     {
-        content memory c= publicLib[arrayID];
-        if(msg.value!=c.price)
+        content memory c= publicLib[_arrayID];
+        uint256 feed = ((c.price * 10 ** 18) / getLatestPrice() * 1 wei);
+        if(msg.value!=feed)
         {
-            revert incorrect_ether();
+            revert incorrectPrice({
+                price: feed,
+                paid: msg.value
+            });
         }
-        userLib[msg.sender][c.ID]=content(c.ID,c.name,c.Link,c.description,c.seller,c.price);
-        privlib[msg.sender].push(content(c.ID,c.name,c.Link,c.description,c.seller,c.price));
+        userLib[msg.sender][c.ID]=content(c.ID,c.name,c.Link,c.description, c.category, c.seller,c.price);
+        privlib[msg.sender].push(content(c.ID,c.name,c.Link,c.description, c.category, c.seller,c.price));
         emit bought(c.seller, msg.sender, c.name, c.price);
-        payable(msg.sender).transfer((msg.value*95)/100);
+        payable(msg.sender).transfer((feed*95)/100);
     }
 
-    function changeOwner(address addr)external
+    /*
+    *@notice front end gets ether value of an item for sale
+    *@param arrayID the position of the item in the public items array
+    */
+    function assetPrice(uint256 _arrayID)external view returns(uint256)
     {
-        if(msg.sender!= Owner)
-        {
-            revert not_owner();
-        }
-        Owner= addr;
+        content memory c= publicLib[_arrayID];
+        uint256 feed = (c.price / getLatestPrice() * 10 ** 18);
+        return feed;
     }
 
+     /*
+     *@notice Returns the latest price
+     */
+    function getLatestPrice() public view returns (uint) {
+        (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return uint(price);
+    }
+
+     /*
+     *@notice changes the owners address
+     *@param addr new owner address
+     */
+    function changeOwner(address _addr)external
+    {
+        if(msg.sender!= owner)
+        {
+            revert not_owner({
+                rightAddress: owner,
+                yourAddress: msg.sender
+            });
+        }
+        owner = _addr;
+    }
+
+     /**
+     *@notice Withdraws contract Revenue to owner
+     */
     function withdraw()external
     {
-        if(msg.sender!= Owner)
+        if(msg.sender != owner)
         {
-            revert not_owner();
+            revert not_owner({
+                rightAddress: owner,
+                yourAddress: msg.sender
+            });
         }
         payable(msg.sender).transfer(address(this).balance);
     }
